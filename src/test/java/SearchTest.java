04bef6551a339efbdcd617f9f10c5e4dd285e438
Andrey <andrejkotara730@gmail.com>
2026-04-30 15:36:29 +1000

    create SearchTest to test Search functionality

---
+++ src/test/java/SearchTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class SearchTest extends BaseTest{
+
+    @Test
+    public void testSearch() {
+        driver.get("https://www.amazon.com/");
+        MainPage mainPage = new MainPage(driver);
+        String query = "lego 21276";
+        SearchResultsPage resultsPage = mainPage.makeQuery(query);
+        String description = resultsPage.fetchFirstCard().getText().toLowerCase();
+        boolean mathesQuery = Arrays.stream(query.split("\\s+")).allMatch(description::contains);
+        Assertions.assertTrue(mathesQuery, "Item matches the query. Search works correctly");
+    }
+
+}
