804be176c05d89bc8bfff0550b8cb62ad289c0b0
Andrey <andrejkotara730@gmail.com>
2026-05-19 17:28:50 +1000

    change local driver to remote driver

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,12 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BaseTest {
 
@@ -11,8 +17,13 @@ public class BaseTest {
 
     @BeforeAll
     public static void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Andrey\\tools\\chromedriver-win64\\chromedriver.exe");
-        driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        String hubUrl = "http://localhost:4444";
+        try {
+            driver = new RemoteWebDriver(new URL(hubUrl), chromeOptions);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Некорректный URL-адрес хаба Selenium Grid: " + hubUrl, e);
+        }
     }
 
     @AfterAll
